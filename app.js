1998971769b3857ccba6f9edcaeb56a46e3de17c
coffee-marios <mark_theo@yahoo.com>
2026-07-01 11:21:15 +0300

    Add ejs, index.ejs and assets

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", { links: links });
 });
 
 app.listen(PORT, () => {
